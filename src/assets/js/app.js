6f07ada4217ed347f4f90603ab08bf14af00cf96
kweydt <kweydt@gmail.com>
2016-12-28 16:21:48 -0800

    adding fonts, project changer, etc

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+$('#project-switching-interface').hide();
+
+$('.project-switcher').on("click", function() {
+  var curText = $(this).text();
+  $('#project-switching-interface').slideToggle(500);
+  if (curText == "Check out a different project") {
+    $(this).text("Close project list");
+  } else {
+    $(this).text("Check out a different project");
+  }
+});
